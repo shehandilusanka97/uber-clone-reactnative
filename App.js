9587eecd90ac2d464c50003db1a314222b26cf02
shehandilusanka97 <dilumzsanox9@gmail.com>
2023-05-17 14:04:30 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+// Set up Redux 
 
 export default function App() {
   return (
+    <Provider store={store}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let's Build Uber</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
